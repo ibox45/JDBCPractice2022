a7b0850aac2b6e8bafb96cb1c91a2ab5b56f348a
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-05-25 20:02:25 +0300

    25.05.22

--- src/main/java/P2_ReadData_Different.java
+++ src/main/java/P2_ReadData_Different.java
@@ -2,21 +2,85 @@ import java.sql.*;
 
 public class P2_ReadData_Different {
 
-        //MySql de day5 de..
-
-        // ilk satirdaki ilk objeyi yazdiralim
-
-        // ikinci satirdaki ilk objeyi yazdiralim
-
-        // ilk sutundaki 5.degeri yazdiralim
+        //MySql de day5 de..personel tablosu
 
 
-        // 0.satira gitmemizin sebebi while sorgusu icerisinde bulunan resultSet.next() in bizi
-        // birinci kayda goturmesi icindir. Eger 1.satira gidecek olsak while icindeki kod yuzunden yazdirmaya
-        // 2.satirdan baslar, tum listeyi alamayiz
+        public static void main(String[] args) throws SQLException, ClassNotFoundException {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/batch60_61?serverTimezone=UTC", "root", "elif.12345");
 
-        // Tum listeyi yazdirmak istersek;
+            Statement st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+            // ilk satirdaki ilk objeyi yazdiralim
+            String query = "Select * from personel";
+            ResultSet rs = st.executeQuery(query);
+            System.out.println(rs.getRow());
 
+            // ikinci satirdaki ilk objeyi yazdiralim
+
+            rs.first();
+            Object ilkSatirdakiPbje = rs.getObject(1);
+            System.out.println("ilkSatirdakiPbje.toString() = " + ilkSatirdakiPbje.toString());
+            
+            // ilk sutundaki 5.degeri yazdiralim
+
+            rs.absolute(5);
+            Object besinciDeger = rs.getObject(1);
+            System.out.println("besinciDeger.toString() = " + besinciDeger.toString());
+
+            // 0.satira gitmemizin sebebi while sorgusu icerisinde bulunan resultSet.next() in bizi
+            // birinci kayda goturmesi icindir. Eger 1.satira gidecek olsak while icindeki kod yuzunden yazdirmaya
+            // 2.satirdan baslar, tum listeyi alamayiz
+
+            // Tum listeyi yazdirmak istersek;
+            rs.absolute(0);
+            while(rs.next()){
+                Object idler = rs.getObject(1);
+                System.out.println(idler.toString());
+            }
+
+            System.out.println("-----------");
+
+            rs.absolute(0);
+            while(rs.next()){
+                Object isimler = rs.getObject("isim");
+                System.out.println(isimler.toString());
+            }
+
+            System.out.println("-----------");
+
+            rs.absolute(0);
+            while(rs.next()){
+                Object sirketler = rs.getObject("sirket");
+                System.out.println(sirketler.toString());
+            }
+
+            System.out.println("-----------");
+
+            rs.absolute(0);
+            while(rs.next()){
+                Object sehirler = rs.getObject("sehir");
+                System.out.println(sehirler.toString());
+            }
+
+            System.out.println("-----------");
+
+            rs.absolute(0);
+            while(rs.next()){
+                Object maaslar = rs.getObject(4);
+                System.out.println(maaslar.toString());
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
 
 }
 
--- src/main/java/P5_CRUD.java
+++ src/main/java/P5_CRUD.java
@@ -17,6 +17,46 @@ public class P5_CRUD {
     // bu class private degisken + constructor() + getter() + setter() ve toString() ler iceren class
 
 
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        String url = "jdbc:mysql://localhost:3306/batch60_61?serverTimezone=UTC";
+        Connection con = DriverManager.getConnection(url, "root", "elif.12345");  //database baglantisi icin
+
+        Statement st = con.createStatement();  //query calistirmak icin
+
         // SORU: Dersler adında bir tablo olusturalım (id int, ders_isim varchar(15), egitim_suresi int)
 
+        String createQuery = "create table dersler (id int, ders_isim VARCHAR(15), egitim_suresi_gun int)";
+
+        st.execute(createQuery);
+        System.out.println("tablo olusturuldu..");
+
+        List<Ders> dersler = new ArrayList<>();
+        dersler.add(new Ders (100, "Java",55));
+        dersler.add(new Ders (101, "SQL",10));
+        dersler.add(new Ders (102, "JDBC",3));
+        dersler.add(new Ders (103, "APi",12));
+        dersler.add(new Ders (104, "JENKİNS",2));
+        dersler.add(new Ders (105, "Selenium",25));
+        dersler.add(new Ders (106, "Appium",10));
+        dersler.add(new Ders (107, "SDLC",10));
+
+        PreparedStatement table = con.prepareStatement("INSERT INTO dersler VALUES (?,?,?)");
+
+        for (Ders d : dersler){
+            table.setInt(1, d.getId());
+            table.setString(2, d.getDers_isim());
+            table.setInt(3, d.getEgitim_suresi());
+
+            table.addBatch();
+        }
+
+        table.executeBatch();
+
+
+        System.out.println("Hersey Tamam");
+
     }
+
+}
