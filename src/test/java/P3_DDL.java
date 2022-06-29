f85a708f618ec72c7b7987e17291ee1245899e7d
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-06-29 17:30:21 +0300

    29.06.22

--- src/test/java/P3_DDL.java
+++ src/test/java/P3_DDL.java
@@ -32,52 +32,31 @@ public class P3_DDL {
 
         // SORU: markalar adında bir tablo oluşturunuz. marka_id int, marka_isim VARCHAR(15), calisan_sayisi int
 
-        String query = "create table markalar (marka_id int, marka_isim VARCHAR(15), calisan_sayisi int)";
-
 
         //1.yontem : execute () methodu ile
 
-        boolean s1 = st.execute(query);
-        System.out.println("markalar tablosu olusturuldu ->" +s1); //markalar tablosu olusturuldu ->false
-
-        // execute(), Boolean bir deger dondurur.
-        //- DDL islemlerin false dondururken(ResultSet olusturmadigi icin),  DML islemlerinde true deger dondurur.(ResultSet olusturur)
 
         //2.yontem : executeUpdate() methodu ile
 
-     //   int s2 = st.executeUpdate(query);
-     //   System.out.println("markalar tablosu olusturuldu ->" +s2); //markalar tablosu olusturuldu ->0
 
         // SORU: markalar tablosunu siliniz
-
-    //    String dropQuery = "Drop table markalar";
-    //    st.execute(dropQuery);
-    //    System.out.println("marklar tablosu silindi...");
 
 
         // SORU : markalar tablosuna yeni bir sutun {sube_sayisi int} ekleyiniz
 
-    //    String alterQuery = "ALTER table markalar ADD sube_sayisi int";
-    //    st.execute(alterQuery);
 
         // SORU : markalar tablosuna yeni bir sutun {sube_sayisi int} ekleyiniz, ancak bu sutunun yeri marka_id den sonra olsun
 
-    //    String alterQuery2 = "ALTER table markalar ADD sube_sayisi int AFTER marka_id";
-    //    st.execute(alterQuery2);
 
         // SORU : markalar tablosunun adini  brands olarak degistiriniz
-    //    String alterQuery3 = "ALTER table markalar RENAME TO brands";
-    //    System.out.println("tablo ismi brands olarak degistirildi.. " + st.execute(alterQuery3));
 
 
         // SORU : markalar tablosunda marka_isim sutununu isim olarak degistiriniz
-    //    String alterQuery4 = "ALTER table markalar RENAME column marka_isim to isim";
-    //    st.execute(alterQuery4);
+
 
         // SORU : markalar tablosunda marka_isim sutununun data type ini char(20) olarak degistiriniz
 
-        st.execute("alter table markalar modify marka_isim char(20)");
-        System.out.println("marka isim data type char(20) oldu");
+
 
     }
 }
--- src/test/java/P5_CRUD.java
+++ src/test/java/P5_CRUD.java
@@ -27,35 +27,6 @@ public class P5_CRUD {
 
         // SORU: Dersler adında bir tablo olusturalım (id int, ders_isim varchar(15), egitim_suresi int)
 
-        String createQuery = "create table dersler (id int, ders_isim VARCHAR(15), egitim_suresi_gun int)";
-
-        st.execute(createQuery);
-        System.out.println("tablo olusturuldu..");
-
-        List<Ders> dersler = new ArrayList<>();
-        dersler.add(new Ders (100, "Java",55));
-        dersler.add(new Ders (101, "SQL",10));
-        dersler.add(new Ders (102, "JDBC",3));
-        dersler.add(new Ders (103, "APi",12));
-        dersler.add(new Ders (104, "JENKİNS",2));
-        dersler.add(new Ders (105, "Selenium",25));
-        dersler.add(new Ders (106, "Appium",10));
-        dersler.add(new Ders (107, "SDLC",10));
-
-        PreparedStatement table = con.prepareStatement("INSERT INTO dersler VALUES (?,?,?)");
-
-        for (Ders d : dersler){
-            table.setInt(1, d.getId());
-            table.setString(2, d.getDers_isim());
-            table.setInt(3, d.getEgitim_suresi());
-
-            table.addBatch();
-        }
-
-        table.executeBatch();
-
-
-        System.out.println("Hersey Tamam");
 
     }
 
