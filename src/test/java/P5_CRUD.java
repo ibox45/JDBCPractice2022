27dd8e0e4b98fb3c1e439755259fa686e3322d16
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-07-01 16:27:02 +0300

    01.07.22

--- src/test/java/P5_CRUD.java
+++ src/test/java/P5_CRUD.java
@@ -26,8 +26,40 @@ public class P5_CRUD {
         Statement st = con.createStatement();  //query calistirmak icin
 
         // SORU: Dersler adında bir tablo olusturalım (id int, ders_isim varchar(15), egitim_suresi int)
+        String createQuery = "create table dersler (id int, ders_isim VARCHAR(15), egitim_suresi_gun int)";
+
+        st.execute(createQuery);
+        System.out.println("tablo olusturuldu..");
+
+        //Not : Ders diye bir pojo class olustururuz,
+        // id(int), ders_isim(String), egitim_suresi(int) instance variable ları olusturup,
+        // Generate den constructor , getter setter ve toString() methodlari cagiririz
+
+        List<Ders> dersler = new ArrayList<>();
+        dersler.add(new Ders (100, "Java",50));
+        dersler.add(new Ders (101, "SQL",10));
+        dersler.add(new Ders (102, "JDBC",2));
+        dersler.add(new Ders (103, "APi",12));
+        dersler.add(new Ders (104, "JENKİNS",1));
+        dersler.add(new Ders (105, "Selenium",25));
+        dersler.add(new Ders (106, "Appium",10));
+        dersler.add(new Ders (107, "SDLC",10));
+
+        PreparedStatement table = con.prepareStatement("INSERT INTO dersler VALUES (?,?,?)");  //hangi formatta ise tablo o verileri dusunerek value ları yazariz
+
+        for (Ders d : dersler){
+            table.setInt(1, d.getId());
+            table.setString(2, d.getDers_isim());
+            table.setInt(3, d.getEgitim_suresi());
+
+            table.addBatch();
+        }
+
+        table.executeBatch();
 
 
+        System.out.println("Hersey Tamam");
+        //mySql de derslerin hizasindaki son simgeye basilirsa tablo SQL de gorunur..
     }
 
 }
